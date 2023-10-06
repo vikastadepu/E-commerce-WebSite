26e1f7b42dfccae6ee47da59ef9569aed4be41b1
vikastadepu <vikastadepuffe@gmail.com>
2023-10-06 00:36:22 -0700

    sql integration

--- src/main/java/dev/vikas/productservice/models/Category.java
+++ src/main/java/dev/vikas/productservice/models/Category.java
@@ -1,5 +1,25 @@
 package dev.vikas.productservice.models;
 
-public class Category extends BaseModel {
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.util.List;
+@Entity
+public class  Category extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/dev/vikas/productservice/models/Product.java
+++ src/main/java/dev/vikas/productservice/models/Product.java
@@ -1,14 +1,22 @@
 package dev.vikas.productservice.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private String title;
     private String description;
     private String image;
+    @ManyToOne
     private Category category;
     private double price;
 }
